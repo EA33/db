8df294c9b51b80ba45483377d65ca37d6273af03
jen89 <jenchik_1989@bk.ru>
2021-09-10 16:56:26 +0700

    two

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,19 +1,42 @@
-import Swiper, { Navigation, Parallax, Controller } from 'swiper/core';
-Swiper.use([Navigation, Parallax, Controller]);
-
+import Swiper, { Navigation, Controller, Pagination, Parallax } from 'swiper';
+Swiper.use([Navigation, Pagination, Controller, Parallax]);
 (() => {
-    new Swiper('.slider-1', {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 2,
-        freeMode: true,
-        watchSlidesProgress: false,
+    let slider2 = new Swiper('.slider-2', {
+        spaceBetween: 15,
+        speed: 1200,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        // var moveToClickedNumber = function (swiper) { swiper.swipeTo(swiper.clickedSlideIndex) }
+        //
+        // autoScrollOffset: 2,
+        // onSlideClick: moveToClickedNumber,
+        // initialSlide: 0,
     });
 
-    new Swiper('.slider-2', {
+    let slider = new Swiper('.slider-1', {
+        // slidesPerGroup: 2,
+        slidesPerView: 1,
         loop: true,
         speed: 1200,
+        parallax: true,
+
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
     });
+    slider2.controller.control = slider;
+    // renderBullet: function (index, className) {
+    //     return '<span class="' + className + '">' + (index + 1) + '</span>';
+    // },
+
+    // slider2.on('slideChange', function () {
+    //     console.log('slider2.realIndex', slider2.realIndex);
+    // });
 
 })();
 
